6b5e8d26fd760fa034034808dd44627685f9bbe8
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-06-23 16:46:12 -0300

    Conditional Rendering of Components by Authentication

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Cadastro from './views/Cadastro/TelaCadastro.vue'
-import Login from './components/login/Login.vue'
-
-</script>
-
 <template>
   <header className="container-root">
     <RouterView></RouterView>
+    <home v-if="token" @token="setToken"/>
+    <login v-else="token" @token="setToken"/>
   </header>
 </template>
+
+
+<script>
+import Login from './components/login/Login.vue'
+import Cadastro from './views/Cadastro/TelaCadastro.vue'
+
+export default {
+  data(){
+    return {
+      token: localStorage.getItem('accessToken')
+    }
+  },
+  components: {
+    login : Login,
+    cadastro : Cadastro
+  },
+  methods: {
+    setToken(e){
+      this.token = e;
+    }
+  }
+}
+
+</script>
 
 
 <style src="@/styles/global.scss" lang="sass"/>
